29bb3a65ee933bb10cd3f60d5f510195b1a60fc8
Yufan CHEN <108385820+CYF2000127@users.noreply.github.com>
2025-01-16 20:39:41 +0800

    Update mix_pretrain_final55.py

--- config/_base_/dataset/mix_pretrain_final55.py
+++ config/_base_/dataset/mix_pretrain_final55.py
@@ -2,13 +2,13 @@ _base_ = ['DEFAULT_TRAIN_DATASET.py']
 
 data_args = dict(
     train=dict(
-        type='ConcatDatasetWithShuffle',  # 使用 ConcatDatasetWithShuffle，即使只有一个数据集
+        type='ConcatDatasetWithShuffle', 
         cfgs=[
-            {{_base_.DEFAULT_TRAIN_DATASET.flickr}}  # 使用 flickr 数据集的配置
+            {{_base_.DEFAULT_TRAIN_DATASET.flickr}}  
         ]
     ),
-    validation=None,  # 如果没有验证集，保持为 None
-    test=None,  # 如果没有测试集，保持为 None
+    validation=None,  
+    test=None,  
 
     # compute_metric 设置
     compute_metric=None,
